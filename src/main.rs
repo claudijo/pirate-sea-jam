bff29fd3cd500cace78796fbd43f704d7b00a216
Claudijo Borovic <claudijo.borovic@gmail.com>
2023-08-21 12:54:40 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+// Bevy code commonly triggers these lints and they may be important signals
+// about code quality. They are sometimes hard to avoid though, and the CI
+// workflow treats them as errors, so this allows them throughout the project.
+// Feel free to delete this line.
+#![allow(clippy::too_many_arguments, clippy::type_complexity)]
+
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("icon.png"),
+        ..Default::default()
+    });
+}
