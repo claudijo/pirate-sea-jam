97b9d90fd06a68491cf76f83123c0ff1699d23bc
claudijo <claudijo.borovic@gmail.com>
2023-11-07 18:25:23 +0200

    Make it possible to fire cannons using left mouse button

--- src/plugins/mouse_input.rs
+++ src/plugins/mouse_input.rs
@@ -3,6 +3,8 @@ use crate::plugins::orbiting_camera::OrbitMotion;
 use crate::resources::player::InputDevice;
 use bevy::input::mouse::MouseMotion;
 use bevy::prelude::*;
+use crate::components::ship::PlayerShip;
+use crate::events::artillery::{AimCannonEvent, FireCannonEvent};
 
 fn orbit_camera(
     mut mouse_motion_event_reader: EventReader<MouseMotion>,
@@ -15,13 +17,31 @@ fn orbit_camera(
     }
 }
 
+fn handle_fire_button(
+    buttons: Res<Input<MouseButton>>,
+    ship_query: Query<Entity, With<PlayerShip>>,
+    mut aim_cannon_event_writer: EventWriter<AimCannonEvent>,
+    mut fire_cannon_event_writer: EventWriter<FireCannonEvent>,
+) {
+    if buttons.just_pressed(MouseButton::Left) {
+        for entity in &ship_query {
+            aim_cannon_event_writer.send(AimCannonEvent(entity));
+        }
+    }
+    if buttons.just_released(MouseButton::Left) {
+        for entity in &ship_query {
+            fire_cannon_event_writer.send(FireCannonEvent(entity));
+        }
+    }
+}
+
 pub struct MouseInputPlugin;
 
 impl Plugin for MouseInputPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(
             Update,
-            (orbit_camera,)
+            (orbit_camera, handle_fire_button)
                 .run_if(resource_exists_and_equals(InputDevice::Mouse))
                 .run_if(in_state(GameState::InGame)),
         );
